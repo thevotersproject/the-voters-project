8e43ee6b1eb4879f5a1728a46b1e1cb56736c064
Abhishek Bhattarai <Abhishek-programs@noreply.github.com>
2021-08-29 01:04:21 +0545

    Connected to Postgres Database

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Admin(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=20)
+    username = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)

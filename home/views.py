5b0fe51c18eb2902fae63551740a129d26e3e5f6
Abhishek Bhattarai <Abhishek-programs@noreply.github.com>
2021-08-29 00:10:13 +0545

    used template initially

--- home/views.py
+++ home/views.py
@@ -3,18 +3,18 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home/home.html')
 
 
 def user(request):
-    return render(request, 'user.html')
+    return render(request, 'home/user.html')
 
 
 def voter(request):
     if request.method == "POST":
         code = request.POST['event_code']
         print(code)
-    return render(request, 'voter.html')
+    return render(request, 'home/voter.html')
 
 
 def login(request):
@@ -25,4 +25,4 @@ def register(request):
     if request.method == "POST":
         username = request.POST['username']
         print(username)
-    return render(request, 'register.html')
+    return render(request, 'home/register.html')

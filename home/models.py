cc19898524603f41dce311cbf06a224c7f26d0e0
Abhishek Bhattarai <Abhishek-programs@noreply.github.com>
2021-08-29 12:50:31 +0545

    Made Initial Migration and template cofig

--- home/models.py
+++ home/models.py
@@ -4,6 +4,11 @@ from django.db import models
 # Create your models here.
 class Admin(models.Model):
     firstname = models.CharField(max_length=100)
-    lastname = models.CharField(max_length=20)
-    username = models.CharField(max_length=20)
-    email = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=100)
+    username = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    role = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.username
